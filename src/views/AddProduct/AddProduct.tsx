68d732854b591cff78ab2023522d4b4fe65094d3
Alvaro Garzón <alvaro.develops@gmail.com>
2023-03-09 00:02:33 +0100

    feat: routing, global style, code cleaning

---
+++ src/views/AddProduct/AddProduct.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Div } from './AddProductStyle'
+
+type Props = {}
+
+const AddProduct = (props: Props) => {
+  return (
+    <Div>AddProduct</Div>
+  )
+}
+
+export default AddProduct
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Div } from './HomeStyle'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <Div>Home</Div>
+  )
+}
+
+export default Home
