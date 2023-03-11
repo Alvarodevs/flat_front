f99793770b2d25f3097c8f844e0c70ec89d1de00
Alvaro Garzón <alvaro.develops@gmail.com>
2023-03-12 00:01:53 +0100

    services folder with api connections

---
+++ src/services/api.tsx
@@ -0,0 +1,48 @@
+import type IFormData from "../interfaces/IFormData";
+
+export const getAllProducts = async (): Promise<unknown> => {
+  const backURL = process.env.REACT_APP_API_URL
+  const path = 'products';
+  try {
+    const resp = await fetch(`${backURL ?? ''}/${path}/`);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const findByQuery = async (query: string): Promise<unknown> => {
+  const backURL = process.env.REACT_APP_API_URL;
+  const path = 'search';
+
+  try {
+    const resp = await fetch(
+      `${backURL ?? ''}/${path}/?query=${query}`
+    );
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const addProduct = async (product: IFormData): Promise<unknown> => {
+  // localhost:8000/api/products/  -POST
+  const backURL = process.env.REACT_APP_API_URL;
+  const path = 'new_product';
+
+  try {
+    const resp = await fetch(`${backURL ?? ''}/${path}/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(product)
+    });
+    const result = await resp.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
